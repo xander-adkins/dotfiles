f611034e23c80cdc348a6774297484ffda5d10c4
Alexander Adkins <alexander.adkins@zapsurgical.com>
2022-11-12 00:10:09 +0100

    Include tree-sitter syntax highlighting

--- init.el
+++ init.el
@@ -1,73 +1,192 @@
-;; init.el -- Emacs Configuration File
+;;; init.el --- MINIMA's configuration entry point.
+;;
+;; Copyright (c) 2022 Alexander Adkins
+;;
+;; Author: Alexander Adkins <alexander.adkins@icloud.com>
+;; URL: https://github.com/xander-adkins/minima
 
-;; System Settings
-(prefer-coding-system 'utf-8)                                               ; Use utf-8 everywhere
+
+
+
+
+;; Use utf-8 everywhere
+(prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
 
-(setq inhibit-startup-message t)                                            ; Disable Emacs welcome screen
-(setq ring-bell-function 'ignore)                                           ; Turn off all alarms (audio/visual bells) 
-(setq undo-limit 6710886400)                                                ; Increase undo limit to 64MB
-(defalias 'yes-or-no-p 'y-or-n-p)                                           ; Answer 'y' or 'n' for text-mode prompts
+;; Increase undo limit to 64MB
+(setq undo-limit 6710886400)
 
-(setq backup-directory-alist                                                ; Place all backups and autosaves in a single directory
-      `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+;; Disable Emacs welcome screen
+(setq inhibit-startup-message t)
 
-;; UI Settings
-(scroll-bar-mode -1)                                                        ; Disable visible scrollbar
-(tool-bar-mode -1)                                                          ; Disable the toolbar
-(tooltip-mode -1)                                                           ; Disable tooltips
-(set-fringe-mode 10)                                                        ; Add a little space around the border 
-(menu-bar-mode -1)                                                          ; Disable the menu bar
-(column-number-mode t)                                                      ; Show column number in footer
-(global-display-line-numbers-mode t)                                        ; Turn on line numbers globally
-(dolist (mode '(org-mode-hook                                               ; Disable line numbers for some modes
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+;; Turn off all alarms (audio/visual bells)
+(setq ring-bell-function 'ignore)
 
+;; Answer 'y' or 'n' for text-mode prompts
+(defalias 'yes-or-no-p 'y-or-n-p)
 
-(set-face-attribute 'default nil :font "Iosevka Term" :height 180)          ; Set typeface and height
-(load-theme 'modus-vivendi)                                                 ; Load custom theme
+;; Write auto-saves and backups to separate directory
+(make-directory "~/.tmp/emacs/auto-save/" t)
+(setq auto-save-file-name-transforms '((".*" "~/.tmp/emacs/auto-save/" t)))
+(setq backup-directory-alist '(("." . "~/.tmp/emacs/backup/")))
+
+;; Do not move the current file while creating backup.
+(setq backup-by-copying t)
+
+;; Write customizations to a separate file instead of this file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)
 
 
-;; Set up Straight.el as emacs package manager
-(defvar bootstrap-version)                                                  ; Codeblock to bootstrap Straight.el
+
+
+
+;; Disable Default Emacs UI Elements
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+
+;; Add padding around the window border
+(set-fringe-mode 10)
+
+;; Show column number in footer
+(column-number-mode t)
+
+;; Set typeface and height
+(set-face-attribute 'default nil :font "Iosevka Term" :height 180)
+
+;; Load custom theme
+(load-theme 'modus-vivendi)
+
+;; Show stray whitespace.
+(setq-default show-trailing-whitespace t)
+
+
+
+
+
+
+;; Bootstrap Straight.el
+(defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+;; Install use-package
+(straight-use-package 'use-package)
 
-;; Evil Package
-(straight-use-package 'evil)                                                ; Extensible VI layer for Emacs 
-(evil-mode 1)                                                               ; Initialise evil-mode
-(straight-use-package 'undo-fu)                                             ; Simple, linear undo with redo for Emacs
-(setq evil-undo-system 'undo-fu)                                            ; Set evil undo to undo-fu package
+;; Configure use-package to use straight.el by default
+(use-package straight
+	 :custom (straight-use-package-by-default t))
 
-(evil-global-set-key 'motion "j" 'evil-next-visual-line)                    ; Next line includes wrapped lines
-(evil-global-set-key 'motion "k" 'evil-previous-visual-line)                ; Previous line includes wrapped lines
-(define-key evil-normal-state-map (kbd "<left>") 'evil-prev-buffer)         ; Navigate to previous buffer on arrow-left key press 
-(define-key evil-normal-state-map (kbd "<right>") 'evil-next-buffer)        ; Navigate to next buffer on arrow-right key press 
-(define-key evil-normal-state-map (kbd "L") 'evil-end-of-line)              ; Jump to end of line 
-(define-key evil-normal-state-map (kbd "H") 'evil-beginning-of-line)        ; Jump to beginning of line 
+
+
+
+
+;; Evil VIM key bindings
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-undo-system 'undo-fu)
+  :config
+  (evil-mode 1))
+
+;; A set of keybindings for evil-mode
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+;; Package for sensible redo support
+(use-package undo-fu)
+
+;; Next line includes wrapped lines
+(evil-global-set-key 'motion "j" 'evil-next-visual-line)
+;; Previous line includes wrapped lines
+(evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+;; Move to right window
+(evil-global-set-key 'motion "C-l" 'evil-window-right)
+;; Move to left window
+(evil-global-set-key 'motion "C-h" 'evil-window-left)
+;; Jump to end of line
+(define-key evil-normal-state-map (kbd "L") 'evil-end-of-line)
+;; Jump to beginning of line
+(define-key evil-normal-state-map (kbd "H") 'evil-beginning-of-line)
+
+
+
+
+
+
+
+;; Syntax Highlighting with Tree Sitter
+(use-package tree-sitter
+  :ensure t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+
+(use-package typescript-mode
+  :after tree-sitter
+  :config
+  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
+  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+
+  ;; use our derived mode for tsx files
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  ;; by default, typescript-mode is mapped to the treesitter typescript parser
+  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
+;; auto-format different source code files extremely intelligently
+;; https://github.com/radian-software/apheleia
+(use-package apheleia
+  :config
+  (apheleia-global-mode +1))
+
+
+;; Eglot for code intelligence
+(use-package eglot)
+
 
 
 ;; Which-key Package
-(straight-use-package 'which-key)                                           ; Displays available keybindings in popup
-(which-key-mode)                                                            ; Initialise which-key-mode
-(setq which-key-idle-delay 0.3)                                             ; Delay which-key popup
+(straight-use-package 'which-key)
+(which-key-mode)
+(setq which-key-idle-delay 0.3)
 
-;; Magit Package
-(straight-use-package 'magit)                                               ; Text-based user interface to Git
+;; Magit text based GIT interface
+(use-package magit)
 
+
+
+
+
+
+;; Quick access to init.el
+(defun init-file ()
+  "Edit the `user-init-file', in another window."
+  (interactive)
+  (find-file-other-window user-init-file))
